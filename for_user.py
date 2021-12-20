8e0652cec5bf831955de0de201ca9645449a5e48
IDmitryA <ivan4ik1986@gmail.com>
2021-12-20 23:46:43 +0200

    add try/except

--- for_user.py
+++ for_user.py
@@ -12,31 +12,39 @@ from current_rate import usd_rate, uah_rate
 
 while True:
     command: str = input('enter command:\n')
+    command = command.strip()  # for cases when user accidentally types extra ' ' at the beginning or the
+    # end of a string
     split_command: list = command.upper().split(' ')  # using upper() for any types of user input: 'uAH', 'uAh',
     # 'UsD', 'usD', 'coURsE', 'ExCHangE', 'sTop', etc.
 
     if command.upper() == 'STOP':
         break
 
-    if split_command[0] + ' ' + split_command[1] == 'COURSE UAH':
-        print(get_uah_information())
-    elif split_command[0] + ' ' + split_command[1] == 'COURSE USD':
-        print(get_usd_information())
-    elif all([split_command[0] == 'COURSE', split_command[1] not in ['UAH', 'USD']]):
-        print(f'INVALID CURRENCY {split_command[1]}')
+    try:
+        if split_command[0] + ' ' + split_command[1] == 'COURSE UAH':
+            print(get_uah_information())
+        elif split_command[0] + ' ' + split_command[1] == 'COURSE USD':
+            print(get_usd_information())
+        elif all([split_command[0] == 'COURSE', split_command[1] not in ['UAH', 'USD']]):
+            print(f'INVALID CURRENCY {split_command[1]}')
 
-    if split_command[0] + ' ' + split_command[1] == 'EXCHANGE UAH':  # selling USD
-        if balance.usd_balance >= int(split_command[2]) / usd_rate:  # enough USD for complete exchange
-            print(exchange_uah(int(split_command[2])))
-        else:                                                        # not enough USD for complete exchange
-            print(f'UNAVAILABLE, REQUIRED BALANCE USD {balance.usd_balance},'
-                  f' AVAILABLE {int(split_command[2]) / usd_rate}')
-            break
-    elif split_command[0] + ' ' + split_command[1] == 'EXCHANGE USD':  # buying USD
-        if balance.uah_balance >= int(split_command[2]) * uah_rate:    # enough UAH for complete exchange
-            print(exchange_usd(int(split_command[2])))
-        else:                                                          # not enough UAH for complete exchange
-            print(f'UNAVAILABLE, REQUIRED BALANCE USD {balance.uah_balance},'
-                  f' AVAILABLE {int(split_command[2]) * uah_rate}')
-            break
-
+        elif split_command[0] + ' ' + split_command[1] == 'EXCHANGE UAH':  # selling USD
+            if balance.usd_balance >= float(split_command[2]) / usd_rate:  # enough USD for complete exchange
+                print(exchange_uah(float(split_command[2])))
+            else:                                                        # not enough USD for complete exchange
+                print(f'UNAVAILABLE, REQUIRED BALANCE USD {balance.usd_balance},'
+                      f' AVAILABLE {float(split_command[2]) / usd_rate}')
+                break
+        elif split_command[0] + ' ' + split_command[1] == 'EXCHANGE USD':  # buying USD
+            if balance.uah_balance >= float(split_command[2]) * uah_rate:    # enough UAH for complete exchange
+                print(exchange_usd(float(split_command[2])))
+            else:                                                          # not enough UAH for complete exchange
+                print(f'UNAVAILABLE, REQUIRED BALANCE USD {balance.uah_balance},'
+                      f' AVAILABLE {float(split_command[2]) * uah_rate}')
+                break
+        else:
+            print('You might be wrong, repeat your command:\n')
+            continue
+    except IndexError:
+        print('You might be wrong, repeat your command:\n')
+        continue
